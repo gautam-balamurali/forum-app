db4b085aa155fb6d45c299ad11684525a5047045
Gautam Balamurali <gtmbalamurali@gmail.com>
2023-06-24 22:26:39 +0530

    feat: sorting implemented

--- src/components/layouts/sortbar/Sortbar.jsx
+++ src/components/layouts/sortbar/Sortbar.jsx
@@ -1,7 +1,19 @@
+/* eslint-disable */
+
+import { useForum } from "../../../core/contexts/ForumContext";
 import "./Sortbar.css";
 
 const Sortbar = () => {
-  return <div>Sortbar</div>;
+  const { sortByDateCreated, sortByUpvotes } = useForum();
+  return (
+    <div className="sort-page">
+      <h2>Sort By</h2>
+      <div className="sorting-options">
+        <button onClick={sortByDateCreated}>Sort by Date Created</button>
+        <button onClick={sortByUpvotes}>Sort by Upvotes</button>
+      </div>
+    </div>
+  );
 };
 
 export default Sortbar;
