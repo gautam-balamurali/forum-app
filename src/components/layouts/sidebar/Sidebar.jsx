0a462cc05eb9fa90dd9710a10ce9a1d87689ce74
Gautam Balamurali <gtmbalamurali@gmail.com>
2023-06-24 22:59:01 +0530

    style: sidebar refinement

--- src/components/layouts/sidebar/Sidebar.jsx
+++ src/components/layouts/sidebar/Sidebar.jsx
@@ -1,23 +1,40 @@
 import { FaHome, FaRegUserCircle } from "react-icons/fa";
-import "./Sidebar.css";
 import { BsBookmark, BsRocketFill } from "react-icons/bs";
 
+import "./Sidebar.css";
+import { useForum } from "../../../core/contexts/ForumContext";
+
 const Sidebar = () => {
+  const { forumData } = useForum();
+
   return (
-    <nav className="navigation">
-      <li>
-        <FaHome /> Home
-      </li>
-      <li>
-        <BsRocketFill /> Explore
-      </li>
-      <li>
-        <BsBookmark /> Bookmarks
-      </li>
-      <li>
-        <FaRegUserCircle /> Profile
-      </li>
-    </nav>
+    <div className="sidebar">
+      <nav className="navigation">
+        <li>
+          <FaHome /> Home
+        </li>
+        <li>
+          <BsRocketFill /> Explore
+        </li>
+        <li>
+          <BsBookmark /> Bookmarks
+        </li>
+        <li>
+          <FaRegUserCircle /> Profile
+        </li>
+      </nav>
+      <div className="current-user-details">
+        <img
+          className="user-comment-avatar"
+          src={forumData.picUrl}
+          alt={forumData.username}
+        />
+        <div className="current-user-info">
+          <h4>{forumData.name}</h4>
+          <p>@{forumData.username}</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
